6f7612997a476a7903009d11a1f5ff2acd279912
Mark <markyatsuk12.04.16@gmail.com>
2024-10-12 20:38:09 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
-// Функция для переключения активного слайда
 function changeSlide() {
   const carouselItems = document.getElementsByClassName("carousel-item");
   const activeItem = document.querySelector(".carousel-item.active");
 
-  // Находим индекс активного слайда
   let activeIndex = Array.from(carouselItems).indexOf(activeItem);
 
-  // Удаляем класс 'active' у текущего слайда
-  activeItem.classList.remove("active");
+  window.requestAnimationFrame(() => {
+    activeItem.classList.remove("active");
 
-  // Вычисляем индекс следующего слайда
-  let nextIndex = (activeIndex + 1) % carouselItems.length;
+    let nextIndex = (activeIndex + 1) % carouselItems.length;
 
-  // Добавляем класс 'active' к следующему слайду
-  carouselItems[nextIndex].classList.add("active");
+    carouselItems[nextIndex].classList.add("active");
+  });
 }
-
-// Запускаем функцию changeSlide каждые 7 секунд
 setInterval(changeSlide, 5000);
